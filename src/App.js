e1d2cfbeec862e9783c6e8440ffbae6d15ef2495
Salas <radjefsls@gmail.com>
2020-09-09 01:56:34 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+
 
 function App() {
   return (
-    <div>
-      {/* SideBar */}
-      {/* ChatBoxc*/ }
-      </div>
+    <div className="app">
+      <div className="app__body">
+        <Sidebar/>
+        <Chat/>
+      </div>  
+    </div>
+
   );
 }
 
---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+function Chat(){
+    return(
+        <div className="chat">
+            <h1>This is a chat</h1>
+        </div>
+    )
+}
+
+export default Chat
