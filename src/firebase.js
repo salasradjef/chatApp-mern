fe4e85966b32b9c496156c3500f4ee05e4dd9c77
Salas <radjefsls@gmail.com>
2020-09-09 01:38:11 +0100

    first

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAkAGYyN5z2Bn2EqHHZeBmHY8k2ruTRGjE",
+    authDomain: "whatsapp-mern-f9712.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-f9712.firebaseio.com",
+    projectId: "whatsapp-mern-f9712",
+    storageBucket: "whatsapp-mern-f9712.appspot.com",
+    messagingSenderId: "219252207307",
+    appId: "1:219252207307:web:c96c611eb5e779e843b54e"
+  };
